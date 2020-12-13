da68ed36f2f4a484bc14c05f8a0f79561aa1d92d
rmrbdx <75951069+rmrbdx@users.noreply.github.com>
2020-12-13 14:25:08 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAOOAdnYLRP6Cbu23MPzW-f_jjfZYbzt8M",
+    authDomain: "rickmrbdx11.firebaseapp.com",
+    projectId: "rickmrbdx11",
+    storageBucket: "rickmrbdx11.appspot.com",
+    messagingSenderId: "176423578936",
+    appId: "1:176423578936:web:6208996309a27863b5a5cd",
+    measurementId: "G-545LNSHGF9"
+});
