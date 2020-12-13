941943413b062a631f7f2da8d412701d2c7fec96
rmrbdx <75951069+rmrbdx@users.noreply.github.com>
2020-12-13 14:25:47 -0600

    Create navegacion.js

---
+++ js/navegacion.js
@@ -0,0 +1,11 @@
+export function muestraSesión() {
+  location.href = "index.html";
+}
+
+export function muestraPasatiempos() {
+  location.href = "pasatiempos.html";
+}
+
+export function muestraUsuarios() {
+  location.href = "usuarios.html";
+}
