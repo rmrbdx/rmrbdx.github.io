6063f577d88f07c9852c00f2d908aa345958ba59
rmrbdx <75951069+rmrbdx@users.noreply.github.com>
2021-01-07 13:18:05 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,7 +1,7 @@
 class MiFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /* html */
-            `Copyright &copy; 2020 Ricardo Armando Machorro Reyes.`;
+            `Copyright &copy; 2021 Ricardo Armando Machorro Reyes.`;
     }
 }
 customElements.define("mi-footer", MiFooter);
